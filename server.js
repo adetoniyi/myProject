7d73da801e604381d5e121a5ee6b5617f9c2cdd1
adetoniyi <adetoniyi04@gmail.com>
2025-05-05 10:12:09 +0100

    API Server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const propertyRoutes = require("./routes/propertyRoutes");
+const errorHandler = require("./middlewares/errorHandler");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/properties", propertyRoutes);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
